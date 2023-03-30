7587a3a808b8f5e856efb524f1d2f432bc079188
Mlecznyy <121568980+Mlecznyy@users.noreply.github.com>
2023-03-30 20:10:54 +0200

    Update videos.js

--- js/videos.js
+++ js/videos.js
@@ -1,20 +1,23 @@
 var linki = {
-    "1": "https://file-examples.com/storage/feb401d325641db2fa1dfe7/2017/04/file_example_MP4_1280_10MG.mp4",
+    "1": "https://drive.google.com/file/d/1jBgIk5RPY8OMc91RkZVVruQ06T9o5fUy/view?usp=share_link",
     };
 var tytuly = {
-    "1": "Tak będzie wyglądał 1 film! <span>#test</span>",
+    "1": "Nauka HTML&CSS <span>#nauka_programowania</span>",
     }
 var title = {
-    "1": "Tak będzie wyglądał 1 film!",
+    "1": "Materialy odc 1 | Nauka HTML&CSS",
     }
 var linki_yt = {
+    "1": "https://www.youtube.com/watch?v=zWQI0fJ3xPQ",
+    }
+var linki_dl = {
     "1": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
     }
     window.onload = function() {
     var container = document.getElementById("videos");
     for (var key in linki) {
         if (linki.hasOwnProperty(key)) {
-        container.innerHTML+='<div class="video"><video controls><source src="' + linki[key] + '" type="video/mp4"><source src="' + linki[key] + '" type="video/ogg">Twoja przegladarka nie wspiera HTML video</video><h1>' + tytuly[key] + '</h1><a title="Odtwórz wideo" href="' + linki_yt[key] + '"><span class="material-symbols-outlined">play_circle</span></a><a title="Pobierz ' + title[key] + '" href="' + linki[key] + '" download><span class="material-symbols-outlined">download_for_offline</span></a></div>';
+        container.innerHTML+='<div class="video"><video controls><source src="' + linki[key] + '" type="video/mp4"><source src="' + linki[key] + '" type="video/ogg">Twoja przegladarka nie wspiera HTML video</video><h1>' + tytuly[key] + '</h1><a title="Odtwórz wideo" href="' + linki_yt[key] + '"><span class="material-symbols-outlined">play_circle</span></a><a title="Pobierz ' + title[key] + '" href="' + linki_dl[key] + '" download><span class="material-symbols-outlined">download_for_offline</span></a></div>';
         }
     }
 };
