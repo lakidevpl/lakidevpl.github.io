2fc7ef756bf60b798251010d8bb1d29058c33b5e
Mlecznyy <121568980+Mlecznyy@users.noreply.github.com>
2023-04-29 11:53:35 +0200

    Delete js directory

--- js/fake_load.js
+++
@@ -1,9 +0,0 @@
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-window.onload = async function() {
-    for (let i = 0; i < 3; i++) {
-        await sleep(i * 1000);
-    }
-    location.replace("home.html")
-}
--- js/meta_embed.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementsByTagName("head")[0].innerHTML += '<meta property="og:type" content="website">';
-document.getElementsByTagName("head")[0].innerHTML += '<meta property="og:title" content="Mleczny Programista | Filmy">';
-document.getElementsByTagName("head")[0].innerHTML += '<meta property="og:description" content="Ogladaj filmy na stronie Mlecznego Programisty">';
-document.getElementsByTagName("head")[0].innerHTML += '<meta property="og:url" content="https://mlecznyy.github.io">';
-document.getElementsByTagName("head")[0].innerHTML += '<meta property="og:image" content="https://github.com/Mlecznyy/mlecznyy.github.io/blob/main/images/avatar.png?raw=true">';
-document.getElementsByTagName("head")[0].innerHTML += '<meta name="theme-color" content="#0d1013">';
-document.getElementsByTagName("head")[0].innerHTML += '<meta name="twitter:card" content="summary_large_image">';
