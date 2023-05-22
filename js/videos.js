5c45423a67bdbacfe62f098fa401d8e32db7f549
Mlecznyy <121568980+Mlecznyy@users.noreply.github.com>
2023-05-22 18:14:30 +0200

    Added 3 videos!

--- js/videos.js
+++ js/videos.js
@@ -1,14 +1,23 @@
 var linki = {
     "1": "https://www.youtube.com/embed/zWQI0fJ3xPQ",
     "2": "https://www.youtube.com/embed/ZBPct2cNq5I",
+    "3": "https://www.youtube.com/embed/_xfzE7KrSZ4",
+    "4": "https://www.youtube.com/embed/8r_24WkJpw8",
+    "5": "https://www.youtube.com/embed/0LfvLoEB8Xk",
 };
 var dlinki = {
     "1": "http://lyksoomu.com/16Vwt",
     "2": "http://lyksoomu.com/16lME",
+    "3": "#",
+    "4": "#",
+    "5": "#",
 }
 var title = {
     "1": "Nauka HTML & CSS | #1",
     "2": "Zadania HTML & CSS | #1",
+    "3": "Windows Server | #1",
+    "4": "Windows Server | #2",
+    "5": "Windows Server | #3",
 }
     window.onload = function() {
     var container = document.getElementById("videos");
